7245bed97019f31cc9ff7c5211a2b0c7b5e07867
TimothyMee <timothy33.tf@gmail.com>
2018-02-22 21:04:47 +0100

    little bug fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,136 +1,311 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>RMS</title>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+    <link href="/img/favicon.144x144.html" rel="apple-touch-icon" type="image/png" sizes="144x144">
+    <link href="/img/favicon.114x114.html" rel="apple-touch-icon" type="image/png" sizes="114x114">
+    <link href="/img/favicon.72x72.html" rel="apple-touch-icon" type="image/png" sizes="72x72">
+    <link href="/img/favicon.57x57.html" rel="apple-touch-icon" type="image/png">
+    <link href="/img/favicon.html" rel="icon" type="image/png">
+    <link href="/img/favicon-2.html" rel="shortcut icon">
 
-                        <div class="form-group{{ $errors->has('identification_no') ? ' has-error' : '' }}">
-                            <label for="identification_no" class="col-md-4 control-label">Identification No</label>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="/css/lobipanel/lobipanel.min.css">
+    <link rel="stylesheet" href="/css/vendor/lobipanel.min.css">
+    <link rel="stylesheet" href="/css/jqueryui/jquery-ui.min.css">
+    <link rel="stylesheet" href="/css/pages/widgets.min.css">
+    <link rel="stylesheet" href="/css/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="with-side-menu control-panel control-panel-compact">
 
-                            <div class="col-md-6">
-                                <input id="identification_no" type="text" class="form-control" name="identification_no" value="{{ old('identification_no') }}" required autofocus>
+<header class="site-header">
+    <div class="container-fluid">
+        <a href="" class="site-logo">
+            {{--<img class="hidden-md-down" src="img/logo-2.png" alt="">--}}
+            <img class="hidden-lg-down" src="/img/logo-2-mob.png" alt="">
+        </a>
 
-                                @if ($errors->has('identification_no'))
-                                    <span class="help-block">
+        {{--<button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
+            <span>toggle menu</span>
+        </button>--}}
+
+        <button class="hamburger hamburger--htla">
+            <span>toggle menu</span>
+        </button>
+        <div class="site-header-content">
+            <div class="site-header-content-in">
+                <div class="site-header-shown">
+                    <div class="dropdown user-menu">
+
+                    </div>
+
+                    <button type="button" class="burger-right">
+                        <i class="font-icon-menu-addl"></i>
+                    </button>
+                </div><!--.site-header-shown-->
+
+                <div class="mobile-menu-right-overlay"></div>
+
+            </div><!--site-header-content-in-->
+        </div><!--.site-header-content-->
+    </div><!--.container-fluid-->
+</header><!--.site-header-->
+
+<div class="page-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h2>Register</h2></div>
+
+                    <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('identification_no') ? ' has-error' : '' }}">
+                                <label for="identification_no" class="col-md-4 control-label">Identification No</label>
+
+                                <div class="col-md-6">
+                                    <input id="identification_no" type="text" class="form-control" name="identification_no" value="{{ old('identification_no') }}" required autofocus>
+
+                                    @if ($errors->has('identification_no'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('identification_no') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
-                            <label for="firstname" class="col-md-4 control-label">First Name</label>
+                            <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                                <label for="firstname" class="col-md-4 control-label">First Name</label>
 
-                            <div class="col-md-6">
-                                <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('firstname') }}" required autofocus>
+                                <div class="col-md-6">
+                                    <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('firstname') }}" required autofocus>
 
-                                @if ($errors->has('firstname'))
-                                    <span class="help-block">
+                                    @if ($errors->has('firstname'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('firstname') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
-                            <label for="lastname" class="col-md-4 control-label">Last Name</label>
+                            <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                                <label for="lastname" class="col-md-4 control-label">Last Name</label>
 
-                            <div class="col-md-6">
-                                <input id="lastname" type="text" class="form-control" name="lastname" value="{{ old('lastname') }}" required autofocus>
+                                <div class="col-md-6">
+                                    <input id="lastname" type="text" class="form-control" name="lastname" value="{{ old('lastname') }}" required autofocus>
 
-                                @if ($errors->has('lastname'))
-                                    <span class="help-block">
+                                    @if ($errors->has('lastname'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('lastname') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('middlename') ? ' has-error' : '' }}">
-                            <label for="middlename" class="col-md-4 control-label">Middle Name</label>
+                            <div class="form-group{{ $errors->has('middlename') ? ' has-error' : '' }}">
+                                <label for="middlename" class="col-md-4 control-label">Middle Name</label>
 
-                            <div class="col-md-6">
-                                <input id="middlename" type="text" class="form-control" name="middlename" value="{{ old('middlename') }}" required autofocus>
+                                <div class="col-md-6">
+                                    <input id="middlename" type="text" class="form-control" name="middlename" value="{{ old('middlename') }}" required autofocus>
 
-                                @if ($errors->has('middlename'))
-                                    <span class="help-block">
+                                    @if ($errors->has('middlename'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('middlename') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('tel_no') ? ' has-error' : '' }}">
-                            <label for="tel_no" class="col-md-4 control-label">Telephone Number</label>
+                            <div class="form-group{{ $errors->has('tel_no') ? ' has-error' : '' }}">
+                                <label for="tel_no" class="col-md-4 control-label">Telephone Number</label>
 
-                            <div class="col-md-6">
-                                <input id="tel_no" type="tel_no" class="form-control" name="tel_no" value="{{ old('tel_no') }}" required>
+                                <div class="col-md-6">
+                                    <input id="tel_no" type="tel_no" class="form-control" name="tel_no" value="{{ old('tel_no') }}" required>
 
-                                @if ($errors->has('tel_no'))
-                                    <span class="help-block">
+                                    @if ($errors->has('tel_no'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('tel_no') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <div class="form-group">
+                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
                             </div>
-                        </div>
 
 
-                        <input type="hidden" name ="user_type" value="3">
+                            <input type="hidden" name ="user_type" value="3">
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Register
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+</div><!--.page-content-->
+
+<script src="/js/lib/jquery/jquery-3.2.1.min.js"></script>
+<script src="/js/lib/popper/popper.min.js"></script>
+<script src="/js/lib/tether/tether.min.js"></script>
+<script src="/js/lib/bootstrap/bootstrap.min.js"></script>
+<script src="/https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+<script src="/js/plugins.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.panel').each(function () {
+            try {
+                $(this).lobiPanel({
+                    sortable: true
+                }).on('dragged.lobiPanel', function(ev, lobiPanel){
+                    $('.dahsboard-column').matchHeight();
+                });
+            } catch (err) {}
+        });
+
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var dataTable = new google.visualization.DataTable();
+            dataTable.addColumn('string', 'Day');
+            dataTable.addColumn('number', 'Values');
+            // A column for custom tooltip content
+            dataTable.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
+            dataTable.addRows([
+                ['MON',  130, ' '],
+                ['TUE',  130, '130'],
+                ['WED',  180, '180'],
+                ['THU',  175, '175'],
+                ['FRI',  200, '200'],
+                ['SAT',  170, '170'],
+                ['SUN',  250, '250'],
+                ['MON',  220, '220'],
+                ['TUE',  220, ' ']
+            ]);
+
+            var options = {
+                height: 314,
+                legend: 'none',
+                areaOpacity: 0.18,
+                axisTitlesPosition: 'out',
+                hAxis: {
+                    title: '',
+                    textStyle: {
+                        color: '#fff',
+                        fontName: 'Proxima Nova',
+                        fontSize: 11,
+                        bold: true,
+                        italic: false
+                    },
+                    textPosition: 'out'
+                },
+                vAxis: {
+                    minValue: 0,
+                    textPosition: 'out',
+                    textStyle: {
+                        color: '#fff',
+                        fontName: 'Proxima Nova',
+                        fontSize: 11,
+                        bold: true,
+                        italic: false
+                    },
+                    baselineColor: '#16b4fc',
+                    ticks: [0,25,50,75,100,125,150,175,200,225,250,275,300,325,350],
+                    gridlines: {
+                        color: '#1ba0fc',
+                        count: 15
+                    }
+                },
+                lineWidth: 2,
+                colors: ['#fff'],
+                curveType: 'function',
+                pointSize: 5,
+                pointShapeType: 'circle',
+                pointFillColor: '#f00',
+                backgroundColor: {
+                    fill: '#008ffb',
+                    strokeWidth: 0,
+                },
+                chartArea:{
+                    left:0,
+                    top:0,
+                    width:'100%',
+                    height:'100%'
+                },
+                fontSize: 11,
+                fontName: 'Proxima Nova',
+                tooltip: {
+                    trigger: 'selection',
+                    isHtml: true
+                }
+            };
+
+            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+            chart.draw(dataTable, options);
+        }
+        $(window).resize(function(){
+            drawChart();
+            setTimeout(function(){
+            }, 1000);
+        });
+    });
+</script>
+<script src="/js/app1.js"></script>
+<script src="/js/app.js"></script>
+</body>
+</html>
+
