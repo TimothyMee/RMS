5be8e98a54588231b5edf7f1fecf84f3ff174cbd
TimothyMee <timothy33.tf@gmail.com>
2018-03-08 11:55:06 +0100

    Started with the printing and download

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('home', 'HomeController@index')->name('home');
     Route::get('test', 'HomeController@test');
     Route::get('auth', 'HomeController@auth');
+    Route::get('pdf', 'HomeController@pdf');
     Route::post('saveThemeSetting', 'UserSettingController@add')->name('save_theme');
 
     Route::group(['prefix' => 'course'], function (){
@@ -68,6 +69,13 @@ Route::group(['middleware' => 'auth'], function () {
         });
         Route::group(['prefix' => 'status'], function (){
            Route::post('update', 'ResultController@updateStatus');
+        });
+
+        Route::group(['prefix' => 'pdf'], function (){
+           /*Route::get('index', function (){
+               return '<form method="post" action="/result/pdf/make">'.csrf_field().'<input type="Submit"></form>';
+           });*/
+           Route::get('make', 'PdfController@make');
         });
     });
 
